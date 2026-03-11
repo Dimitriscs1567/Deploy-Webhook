e19c65336aebedac165af7f74ce505078c1ea577
Dimitrios Tikas <tikasdimitrios@gmail.com>
2026-03-11 21:24:24 +0100

    changes

--- scripts/lead-your-way-frontend.sh
+++ scripts/lead-your-way-frontend.sh
@@ -2,5 +2,5 @@
 cd Lead-Your-Way-Frontend
 git pull
 cp -R build/web/* /var/www/Lead-Your-Way-Frontend/
-systemctl reload nginx.service
+systemctl restart nginx.service
 echo "Deployment of Lead-Your-Way-Frontend complete!"
