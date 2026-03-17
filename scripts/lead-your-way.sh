b78536e3112d4ed23a0d398f7a75f1169bc92343
Dimitrios Tikas <tikasdimitrios@gmail.com>
2026-03-17 19:52:29 +0100

    fixes

--- scripts/lead-your-way.sh
+++ scripts/lead-your-way.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+export PATH="$HOME/.local/bin:$PATH"
 cd /var/www/Lead-Your-Way
 git pull
 uv sync
 uv run python manage.py migrate
 uv run python manage.py collectstatic --noinput
-sudo systemctl restart leadyourway.service leadyourway-worker.service
+sudo /bin/systemctl restart leadyourway.service leadyourway-worker.service
 echo "Deployment of Lead-Your-Way complete!"
